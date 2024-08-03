684d79c6549fe804f5787ff69f302039e37da789
Laes Uddin <laesuddin@gmail.com>
2024-08-03 16:36:34 +0600

    Create Parent Layout and extend it into Child

--- resources/views/account/login.blade.php
+++ resources/views/account/login.blade.php
@@ -1,24 +1,6 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Book Review App</title>
-        <link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="{{asset('css/style.css')}}">
-    </head>
-    <body class="bg-light">
-        <div class="container-fluid shadow-lg header">
-            <div class="container">
-                <div class="d-flex justify-content-between">
-                    <h1 class="text-center"><a href="index.html" class="h3 text-white text-decoration-none">Book Review App</a></h1>
-                    <div class="d-flex align-items-center navigation">
-                        <a href="{{route('account.login')}}" class="text-white">Login</a>
-                    <a href="{{route('account.register')}}" class="text-white ps-2">Register</a>
-                    </div>
-                </div>
-            </div>
-        </div>
+@extends('layouts.app')
+
+@section('main')
         <section class=" p-3 p-md-4 p-xl-5">
             <div class="container">
                 <div class="row justify-content-center">
@@ -72,6 +54,4 @@
                 </div>
             </div>
         </section>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    </body>
-</html>
+@endsection
